b86c816644f83f161a7bb9ba398a9050d4cd0ef1
GuestAlexander <119532414+GuestAlexander@users.noreply.github.com>
2023-02-27 17:27:51 -0500

    jkjk

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr. Chocolate", 69, "M", "Brown", "Sassy", 0);
+INSERT INTO bears VALUES (2, "Rowdy", 18, "M", "Brown", "Sassy", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 85, "F", "Brown", "Sassy", 0);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 45, "M", "Brown", "Sassy", 1);
+INSERT INTO bears VALUES (5, "Melissa", 21, "F", "Brown", "Sassy", 0);
+INSERT INTO bears VALUES (6, "Grinch", 89, "M", "Brown", "Sassy", 0);
+INSERT INTO bears VALUES (7, "Wendy", 64, "F", "Brown", "Sassy", 1);
+INSERT INTO bears VALUES (8, null, 25, "M", "Brown", "Sassy", 0);
